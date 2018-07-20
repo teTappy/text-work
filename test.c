61bdab057335c9eff409684947fd4314018e6a0d
teTappy <thetapp1212@gmail.com>
2018-07-20 20:40:06 +1000

    Added some new functions to textstuff.h as well examples on how to call them in test.c

--- test.c
+++ test.c
@@ -3,13 +3,16 @@
 
 int main(void)
 {
+  int x = 1, y = 3;
   char *string = get_string();
+  char *spliced_string = splice(string, x, y);
+  char *reversed_string = reverse_string(string);
+  bool is_palindromic = is_palindrome();
 
-  char *spliced_string = splice(string, 2, 8);
 
-  printf("%s\n", string);
-  printf("%s\n", spliced_string);
-
+  //printf("%s\n", string);
+  printf("Spliced at indeces %i and %i:\n%s\n", x, y, spliced_string);
+  printf("\"%s\" %s\n", reversed_string, is_palindromic ? "is a Palindrome!" : "is NOT a Palindrome");
 
   return 0;
 }
--- textstuff.h
+++ textstuff.h
@@ -2,11 +2,14 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_LENGTH 1000
 
+bool palindrome_flag = false;
+
 // Split the string and return the string between the indeces splice_from and
-// splice_to
+// splice_to (does not alter original string)
 char *splice(char str[], int splice_from, int splice_to)
 {
   int n = strlen(str);
@@ -19,7 +22,7 @@ char *splice(char str[], int splice_from, int splice_to)
   }
   int i, j = 0;
 
-  for (i = splice_from; i < splice_to && i < n; i++)
+  for (i = splice_from; i <= splice_to && i < n; i++)
   {
     spliced_str[j++] = str[i];
   }
@@ -29,17 +32,40 @@ char *splice(char str[], int splice_from, int splice_to)
 }
 
 // This function does exactly what it says, reads input and returns a pointer
-// to the input.
+// to the input. (does not alter original string)
 char *get_string(void)
 {
   printf("input: (%i char max)\n", MAX_LENGTH);
   int c, counter = 0;
   static char input_line[MAX_LENGTH + 1];
 
-
   while (((c = getchar()) != '\n') && counter < MAX_LENGTH)
   {
     input_line[counter++] = c;
   }
   return input_line;
+}
+
+// Does not alter original string, instead returns a pointer to a new 'reversed'
+// string.
+char *reverse_string(char str[])
+{
+  int n = strlen(str);
+  static char reversed_str[MAX_LENGTH];
+  int j = n;
+
+  for (int i = 0; i < n; i++)
+    reversed_str[i] = str[--j];
+
+  reversed_str[n] = '\0';
+  palindrome_flag = strcmp(str, reversed_str) ? false : true;
+
+  return reversed_str;
+}
+
+// reverse_string() must be called prior to is_palindrome(); will
+// return false by default.
+bool is_palindrome(void)
+{
+  return palindrome_flag;
 }
